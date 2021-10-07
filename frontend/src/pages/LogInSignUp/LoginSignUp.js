54ced35ede6595f16b14949efdf2ceed84c84e54
andrespty <andresholee@gmail.com>
2021-10-06 23:33:40 -0400

    Work on loggin page and signup page

--- frontend/src/pages/LogInSignUp/LoginSignUp.js
+++ frontend/src/pages/LogInSignUp/LoginSignUp.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Box, Center, Heading, Input, Button, FormControl, FormLabel, Grid, GridItem } from '@chakra-ui/react'
+import { Box, Center, Heading, Input, Button, FormControl, FormLabel, Grid, GridItem, FormErrorMessage, Collapse, 
+    Alert, AlertDescription, AlertIcon } from '@chakra-ui/react'
 import useLoginSignup from './useLoginSignup'
 
 
 function LoginSignUp({ isLogin }) {
 
-    const { setInfo, submit } = useLoginSignup(isLogin)
+    const { setInfo, submit, loading, error } = useLoginSignup(isLogin)
 
     return (
         <Center h={window.innerHeight}>
@@ -20,22 +21,59 @@ function LoginSignUp({ isLogin }) {
                     }
                 </Heading>
 
+                {
+                    // Alert error when logging in
+                    isLogin
+                    ?<Collapse in={error.detail === '' ? false : true}>
+                        <Alert status='error'>
+                            <AlertIcon/>
+                            <AlertDescription>{error.detail}</AlertDescription>
+                        </Alert>
+                    </Collapse>
+                    : null
+                }
+
                 {/* First Name and Last Name fields */}
                 {
                     isLogin
                     ? null
                     :<Grid templateColumns="repeat(4, 1fr)" gap={1} >
                         <GridItem colSpan={2}>
-                            <Field label='First Name' placeholder='First Name' onChange={(e) => setInfo({'attribute':'first_name', 'value': e.target.value})}/>
+                            <Field 
+                                label='First Name' 
+                                placeholder='First Name' 
+                                error={error.first_name}
+                                onChange={(e) => setInfo({'attribute':'first_name', 'value': e.target.value})}
+                                isInvalid={error.first_name === '' ? false : true}
+                            />
                         </GridItem>
                         <GridItem colSpan={2}>
-                            <Field label='Last Name' placeholder='Last Name' onChange={(e) => setInfo({'attribute':'last_name', 'value': e.target.value})}/>    
+                            <Field 
+                                label='Last Name' 
+                                placeholder='Last Name' 
+                                error={error.last_name}
+                                onChange={(e) => setInfo({'attribute':'last_name', 'value': e.target.value})}
+                                isInvalid={error.last_name === '' ? false : true} 
+                            />    
                         </GridItem>
                     </Grid>
                 }
                 
-                <Field label='Email' placeholder='Email' onChange={(e) => setInfo({'attribute':'email', 'value': e.target.value})}/>
-                <Field label='Password' placeholder='Password' type='password' onChange={(e) => setInfo({'attribute':'password', 'value': e.target.value})}/>
+                <Field 
+                    label='Email' 
+                    placeholder='Email'
+                    error={error.email} 
+                    onChange={(e) => setInfo({'attribute':'email', 'value': e.target.value})}
+                    isInvalid={error.email === '' ? false : true}
+                />
+                <Field 
+                    label='Password' 
+                    placeholder='Password' 
+                    type='password' 
+                    error={error.password}
+                    onChange={(e) => setInfo({'attribute':'password', 'value': e.target.value})}
+                    isInvalid={error.password === '' ? false : true}
+                />
                 
                 {
                     isLogin
@@ -43,13 +81,15 @@ function LoginSignUp({ isLogin }) {
                     : <Field label='Password confirmation' placeholder='Confirm Password' type='password' onChange={(e) => setInfo({'attribute':'password2', 'value': e.target.value})}/>
                 }
                 
-                <Button type='submit'>
+                <Button type='submit' isLoading={loading}>
                     {
                         isLogin
                         ? 'Log In'
                         : 'Sign Up'
                     }
                 </Button>
+
+                <Button onClick={() => console.log(error)}>Click</Button>
 
             </Box>
         </form>
@@ -59,11 +99,12 @@ function LoginSignUp({ isLogin }) {
 
 export default LoginSignUp
 
-const Field = ({ label, placeholder, ...Children }) => {
+const Field = ({ label, placeholder, error, isInvalid, ...Children }) => {
     return(
-        <FormControl isRequired my={2} >
+        <FormControl isRequired my={2} isInvalid={isInvalid}>
             <FormLabel my={0}>{label}</FormLabel>
             <Input placeholder={placeholder} {...Children} />
+            <FormErrorMessage>{error}</FormErrorMessage>
         </FormControl>
     )
 }
--- frontend/src/pages/LogInSignUp/useLoginSignup.js
+++ frontend/src/pages/LogInSignUp/useLoginSignup.js
@@ -1,13 +1,20 @@
-import { useReducer } from "react"
+import { useReducer, useState } from "react"
 import { url } from "../../App"
+import { UserContext } from "../../App"
+import { useContext } from "react"
 
 const useLoginSignup = (isLogin) => {
     const [ info, setInfo ] = useReducer(reducer, initialState)
+    const [ error, setError ] = useReducer(errorReducer, initialStateErrors)
+    const { setUser } = useContext(UserContext)
+    const [ loading, setLoading ] = useState(false)
+
+    const fetchUrl = isLogin ? `${url}/api/token/` : `${url}/api/models/user/`
 
     const submit = (e) => {
         e.preventDefault()
-
-        fetch(`${url}/api/models/user/`, {
+        setLoading(true)
+        fetch(fetchUrl, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -17,12 +24,31 @@ const useLoginSignup = (isLogin) => {
         .then(res => res.json())
         .then(json => {
             console.log(json)
+            if ((isLogin && !json.access) || (!isLogin && !json.token)){
+                console.log('error')
+                setError(json)
+            }
+            else{
+                if ( isLogin ){
+                    let user = JSON.parse(window.atob(json['access'].split('.')[1]))
+                    setUser({type:'loginSignup', ...user})
+                    localStorage.setItem('access', json.access)
+                    localStorage.setItem('refresh', json.refresh)
+                }
+                else{
+                    setUser({type:'loginSignup', ...json})
+                    localStorage.setItem('access', json.token.access)
+                    localStorage.setItem('refresh', json.token.refresh)
+                }
+                setError()
+            }
+            setLoading(false)
         })
 
         console.log('submited')
     }
 
-    return { setInfo, submit }
+    return { setInfo, submit, loading, error }
 }
 
 export default useLoginSignup
@@ -33,6 +59,21 @@ const initialState = {
     email: '',
     password: '',
     password2: ''
+}
+
+const initialStateErrors = {
+    first_name: '',
+    last_name: '',
+    email: '',
+    password: '',
+    detail: ''
+}
+
+const errorReducer = (state, action) => {
+    return {
+        ...initialStateErrors,
+        ...action
+    }
 }
 
 const reducer = (state, action) => {
