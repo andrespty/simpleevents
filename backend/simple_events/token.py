95f71a508d26ec734bc60a13f3e13b1300812f3e
andrespty <andresholee@gmail.com>
2021-10-12 17:01:07 -0400

    Working on adding images to database

--- backend/simple_events/token.py
+++ backend/simple_events/token.py
@@ -10,7 +10,8 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         token['email'] = user.email
         token['first_name'] = user.first_name
         token['last_name'] = user.last_name
-
+        token['id'] = user.id
+        
         return token
 
 class MyTokenObtainPairView(TokenObtainPairView):
