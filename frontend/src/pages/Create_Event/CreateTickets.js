67750adba8bfefbc88784f2a0011797f50d37771
andrespty <andresholee@gmail.com>
2021-10-19 23:58:16 -0400

    Work on ticket addition and functionality

--- frontend/src/pages/Create_Event/CreateTickets.js
+++ frontend/src/pages/Create_Event/CreateTickets.js
@@ -5,22 +5,21 @@ import { Input, FormControl, FormLabel, Box, Stack, Button } from '@chakra-ui/re
 function CreateTickets() {
 
     let { eventID } = useParams()
-    const { ticketList, setInfo, submit } = useCreateTickets(eventID)
+    const { ticketList, submit, add_ticket, setTicketList } = useCreateTickets(eventID)
 
     return (
         <Box>
             <form onSubmit={submit} >
 
-                <TicketField/>
                 {
                     ticketList.map((ticket, key)=>(
-                        <TicketField/>
+                        <TicketField key={key} ticket={ticket} setTicketList={setTicketList} />
                     ))
                 }
 
-                <Button>Add ticket</Button>
+                <Button onClick={add_ticket} >Add ticket</Button>
 
-                <Button>Save tickets</Button>
+                <Button onClick={submit} >Save tickets</Button>
 
             </form>
         </Box>
@@ -29,18 +28,36 @@ function CreateTickets() {
 
 export default CreateTickets
 
-const TicketField = () => {
+const TicketField = React.memo(({ticket, setTicketList}) => {
+
+    console.log(ticket)
+    const remove = (id) => {
+        setTicketList({type:'remove', id:id})
+    }
+
     return(
-        <Stack direction='row' align='center' >
+        <Stack direction='row' align='center'>
+
             <Field isRequired={true} label='Ticket type'>
-                <Input />
+                <Input 
+                    value={ticket.name}
+                    onChange={(e) => setTicketList({type:'modify', id:ticket.id, value:e.target.value, att:'name'})}
+                />
             </Field>
+
             <Field isRequired={true} label='Price'>
                 <Input type='number' />
             </Field>
+
+            {
+                ticket.id === 1
+                ? null
+                :<Button onClick={() => remove(ticket.id)} >Remove</Button>
+            }
+
         </Stack>
     )
-}
+})
 
 const Field = ({ label, placeholder, error, isInvalid, children, isRequired, ...props }) => {
     return(
--- frontend/src/pages/Create_Event/useCreateTickets.js
+++ frontend/src/pages/Create_Event/useCreateTickets.js
@@ -5,6 +5,8 @@ const useCreateTickets = (eventID) => {
     
     const [ ticketList, setTicketList ] = useReducer(reducer, initialInfo)
     const [ id, setId ] = useState(0)
+    const [ ticketNumber, setTicketNumber ] = useState(1)
+
     
     useEffect(() => {
         console.log(eventID)
@@ -14,20 +16,59 @@ const useCreateTickets = (eventID) => {
         })
     },[])
 
+    const add_ticket = () => {
+        setTicketList({type:'add', id:ticketNumber + 1})
+        setTicketNumber(state => state + 1)
+    }
+        
     const submit = () => {
         console.log(id)
+        console.log(ticketNumber)
+        console.log(ticketList)
     }
 
-    return { ticketList, setTicketList, submit }
+    return { ticketList, setTicketList, submit, add_ticket }
 }
 
 export default useCreateTickets
 
-const initialInfo = []
+const initialInfo = [
+    {
+        id: 1,
+        name:'',
+        price:''
+    }
+]
 
 const reducer = (state, action) => {
-    return {
-        ...state,
-        ...action
+
+    switch(action.type){
+
+        case 'add':
+            return [
+                ...state,
+                {
+                    id: action.id,
+                    name:'',
+                    price:''
+                }
+            ]
+
+        case 'remove':
+            var newState = state.filter(ticket => ticket.id !== action.id)
+            return [
+                ...newState
+            ]
+
+        case 'modify':
+            let object = state.filter(ticket => ticket.id === action.id)[0]
+            object[action.att] = action.value
+            return [
+                ...state,
+            ]
+
+        default:
+            return initialInfo
+
     }
 }
